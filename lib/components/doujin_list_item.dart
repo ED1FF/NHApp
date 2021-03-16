c0a9b276b431f0d28b5795b8ba1f99ec0563cf6e
ED1FF <alekarp11@gmail.com>
2021-03-16 20:45:04 +0200

    Add temp doujin list item

---
+++ lib/components/doujin_list_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:nh_android_app/models/doijin.dart';
+
+class DoujinListItem extends StatelessWidget {
+  final Doujin doujin;
+  const DoujinListItem(this.doujin);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          child: Column(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(20),
+                ),
+                child: Image.network(
+                  doujin.thumbnail,
+                  fit: BoxFit.fitWidth,
+                  width: MediaQuery.of(context).size.width * 0.90,
+                  height: 230,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Text(
+                  doujin.title,
+                  style: TextStyle(fontWeight: FontWeight.w700),
+                ),
+              ),
+            ],
+          ),
+          width: MediaQuery.of(context).size.width * 0.90,
+          margin: EdgeInsets.only(top: 15),
+          height: 300,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.black12,
+            ),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/pages/doujins_list_page.dart
+++ lib/pages/doujins_list_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nh_android_app/components/doujin_list_item.dart';
+import 'package:nh_android_app/models/doijin.dart';
 
 class DoujinsListPage extends StatelessWidget {
   const DoujinsListPage({Key key}) : super(key: key);
@@ -10,8 +12,21 @@ class DoujinsListPage extends StatelessWidget {
         appBar: AppBar(
           title: const Text('NHApp'),
         ),
-        body: const Image(
-          image: NetworkImage('https://i.nhentai.net/galleries/1855314/1.jpg'),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              DoujinListItem(Doujin(3, 3, 'Test doujin',
+                  'https://i.nhentai.net/galleries/1855314/1.jpg')),
+              DoujinListItem(Doujin(3, 3, 'Test doujin',
+                  'https://i.nhentai.net/galleries/1855314/1.jpg')),
+              DoujinListItem(Doujin(3, 3, 'Test doujin',
+                  'https://i.nhentai.net/galleries/1855314/1.jpg')),
+              DoujinListItem(Doujin(3, 3, 'Test doujin',
+                  'https://i.nhentai.net/galleries/1855314/1.jpg')),
+              DoujinListItem(Doujin(3, 3, 'Test doujin',
+                  'https://i.nhentai.net/galleries/1855314/1.jpg')),
+            ],
+          ),
         ),
       ),
     );
